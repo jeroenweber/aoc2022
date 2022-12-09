cafd6ecbd752e1f6df56c7f9c14cce37f7d2fd8b
jeroenweber <jeroen.weber@hu.nl>
2022-12-09 13:07:28 +0100

    generelized executeqyery

--- day0501.py
+++ day0501.py
@@ -1,22 +1,54 @@
 from readfromfile import readfromfile
-from queue import Queue
 
 data = readfromfile('C:/Users/jeroen.weber/PycharmProjects/aoc2022g/day0501test.txt')
-message = ''
-
-qm = Queue()
+numberofcolumns = 0
+datamap = list()
 
 def createmap(data):
+    global numberofcolumns
     index = 0
     total = len(data)
     line = data[index]
+    columns = list()
+    col = list()
+    count = []
     while line != '':
-        print(line)
+        pos = 0
+        length = len(line)-1
+        for c in line:
+            if (pos == length):
+                columns.append(col)
+                col = list()
+            if (pos%4 == 1):
+                col.append(c)
+            pos += 1
+            if (c == '1'):
+                count = line
+                break
         data.pop(0)
         line = data[index]
-    #remove empty line
+    numberofcolumns = count[-1]
     data.pop(0)
+    index = len(columns)-1
+    #col = columns.pop(index)
+    #print(col)
+    # for line in columns:
+    #     if
+    return columns
 
-createmap(data)
+
+datamap = createmap(data)
+newmap = list()
+numberofcolumns = int(numberofcolumns)
+for row in datamap:
+    length = len(row)
+    if (length < numberofcolumns):
+        for i in range(length,numberofcolumns):
+            row.append(' ')
+    newmap.append(row)
+
+print(newmap)
 print(data)
+
+
 
